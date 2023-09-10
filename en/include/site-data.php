7c25101ba850e096b19d2d1f4a587810873e8503
Oguz Kaan Cagatay Kilinc <okckilinc@gmail.com>
2023-09-10 03:44:17 +0300

    Create site-data.php

---
+++ en/include/site-data.php
@@ -0,0 +1,13 @@
+<?php
+	$affectedPeople = "520.000";
+
+	# membership fees
+	$membershipFees = array(
+		"entry_fee" => 150,
+		"annual_dues" => 100,
+		"legal_entity_entry_fee" => 500,
+		"legal_entity_annual_dues" => 1000,
+		"student_entry_fee" => 50,
+		"student_annual_dues" => 50
+	);
+>
