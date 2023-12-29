93137547fc63be5896fa36500dc049abbb9f3a01
Oguz Kaan Cagatay Kilinc <okckilinc@gmail.com>
2023-12-29 21:36:48 +0300

    acikacik logo ve link duzeltmesi

--- en/include/header.php
+++ en/include/header.php
@@ -1,41 +1,45 @@
 <header>
-    <nav class="navbar navbar1">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="navbar1-wrapper">
-                        <div class="logoarea"><a href="/en/index.php" title="Sen De Gel"><img src="/assets/images/logo.png" alt="Sen De Gel" width="185" class="img-responsive"></a></div>
-                        <ul class="navbar-nav">
-                            <li class="dropdown">
-                                <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown">WHO WE ARE<i class="hidden-md hidden-lg icon icon-plus"></i></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="/en/who-we-are.php">OUR STORY</a></li>
-                                    <li><a href="/en/our-team.php">OUR TEAM</a></li>
-                                    <li><a href="/en/our-supporters.php">OUR SUPPORTERS</a></li>
-                                    <li><a href="/en/reports.php">ASSOCIATION REPORTS</a></li>
-                                    <li><a href="/en/faq.php">FAQ</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="/en/projects.php">OUR PROJECTS</a></li>
-                            <li class="dropdown">
-                                <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown">NEWS<i class="hidden-md hidden-lg icon icon-plus"></i></a>
-                                <ul class="dropdown-menu">
-                                    <li><a href="/en/news.php" title="News">NEWS</a></li>
-                                    <li><a href="/en/videos.php" title="Videos">VIDEOS</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="/en/become-a-member.php" title="Become a Member">JOIN US</a></li>
-                            <li><a href="/en/contact.php" title="Contact Us">CONTACT US</a></li>
-                            <li>
-                                <a href="../en/make-a-donation.php#section9" class="dropdown-toggle btn btn-secondary">Donate<i class="hidden-md hidden-lg icon icon-plus"></i></a>
-                            </li>
-                            <li><a href="/index.php">TR</a></li>
-                            <li class="stk-logo"><a href="https://acikacik.org/sivil-toplum-kurulusu/sosyal-ve--ekonomik-yasamda-nitelikli-degisim-ve-gelisime-destek-dernegi" target="_blank"><img src="/assets/images/content/stk.png" alt="" width="80px"></a></li>
-                        </ul>
-                        <a href="javascript:;" class="navbar-nav-toggle hidden-md hidden-lg"><i class="icon icon-menu"></i></a>
-                    </div>
-                </div>
-            </div>
+  <nav class="navbar navbar1">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="navbar1-wrapper">
+            <div class="logoarea"><a href="/en/index.php" title="Sen De Gel"><img src="/assets/images/logo.png" alt="Sen De Gel" width="185" class="img-responsive"></a></div>
+            <ul class="navbar-nav">
+              <li class="dropdown">
+                <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown">WHO WE ARE<i class="hidden-md hidden-lg icon icon-plus"></i></a>
+                <ul class="dropdown-menu">
+                  <li><a href="/en/who-we-are.php">OUR STORY</a></li>
+                  <li><a href="/en/our-team.php">OUR TEAM</a></li>
+                  <li><a href="/en/our-supporters.php">OUR SUPPORTERS</a></li>
+                  <li><a href="/en/reports.php">ASSOCIATION REPORTS</a></li>
+                  <li><a href="/en/faq.php">FAQ</a></li>
+                </ul>
+              </li>
+              <li><a href="/en/projects.php">OUR PROJECTS</a></li>
+              <li class="dropdown">
+                <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown">NEWS<i class="hidden-md hidden-lg icon icon-plus"></i></a>
+                <ul class="dropdown-menu">
+                  <li><a href="/en/news.php" title="News">NEWS</a></li>
+                  <li><a href="/en/videos.php" title="Videos">VIDEOS</a></li>
+                </ul>
+              </li>
+              <li><a href="/en/become-a-member.php" title="Become a Member">JOIN US</a></li>
+              <li><a href="/en/contact.php" title="Contact Us">CONTACT US</a></li>
+              <li>
+                <a href="../en/make-a-donation.php#section9" class="dropdown-toggle btn btn-secondary">Donate<i class="hidden-md hidden-lg icon icon-plus"></i></a>
+              </li>
+              <li><a href="/index.php">TR</a></li>
+              <li class="stk-logo">
+                <a href="https://acikacik.org/stk/sivil-toplum-kurulusu/sosyal-ve--ekonomik-yasamda-nitelikli-degisim-ve-gelisime-destek-dernegi" target="_blank" title="Açık Açık NGO">
+                  <img src="/assets/images/content/destekcilerimiz/acikacik.svg" alt="Açık Açık" width="80px"></a>
+                </a>
+              </li>
+            </ul>
+            <a href="javascript:;" class="navbar-nav-toggle hidden-md hidden-lg"><i class="icon icon-menu"></i></a>
+          </div>
         </div>
-    </nav>
+      </div>
+    </div>
+  </nav>
 </header>
