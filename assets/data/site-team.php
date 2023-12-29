77c2e059d94a659e0503283e8761f83c948960f1
Oguz Kaan Cagatay Kilinc <okckilinc@gmail.com>
2023-12-29 22:23:18 +0300

    team coding

---
+++ assets/data/site-team.php
@@ -0,0 +1,51 @@
+<?php
+
+# team
+$team = array(
+	array(
+		'name' => 'İbrahim Betil',
+		'title' => array(
+			'tr' => 'YK Başkanı',
+			'en' => 'Chairman of the Board'
+		),
+		'bio' => array(
+			'tr' => '1994 yılına kadar, Türkiye\'de ve yurt dışında çeşitli sınai ve mali kuruluşların, ticari bankaların genel müdürlüğünü, yönetim kurulu başkanlığını yaptı, kuruculuğunu üstlendi. 1994 yılından sonra Türkiye’de ve yurt dışında eğitim ve sivil toplum çalışmalarıyla ilgilendi ve yönetimlerinde bulundu. Enka Okulları’nın, Toplum Gönüllüleri Vakfı’nın kurucuları arasında yer aldı. Hâlen pek çok dernek ve vakıfta yönetim kurulu üyesi /mütevelli heyet üyesi/denetim kurul üyesi gibi gönüllü sorumluklar üstlenmekte, ayrıca çeşitli sanayi şirketlerinde yönetim kurulu üyeliği yapmaktadır. SenDeGel Derneği’nin kurucularından olup hâlen derneğin yönetim kurulu başkanıdır.',
+			'en' => 'Until 1994, he served as the general manager, chair of the board of directors or the founder of various industrial and financial institutions and commercial banks inside and outside Turkey. After 1994, he has been involved in education and civil society activities inside and outside the country and has been in their management. He is among the founders of Enka Schools and Community Volunteers Foundation (TOG). He still undertakes voluntary responsibilities such as a member of the board of directors, member of the board of trustees, member of the supervisory board in many associations and foundations, and also serves as a member of the board of directors in various industrial companies. He is one of the founders of the Sen De Gel association and is currently the chair of the association\'s board of directors.'
+		),
+		'type' => array(
+			array(
+				'type' => 'founder',
+				'order' => 3
+			),
+			array(
+				'type' => 'board-of-directors',
+				'order' => 1
+			)
+		)
+	),
+	array(
+		'name' => 'Aziz Akgül',
+		'title' => array(
+			'tr' => 'Yedek Üye',
+			'en' => 'Alternate Member'
+		),
+		'bio' => array(
+			'tr' => 'Kara Harp Okulu işletme bölümü mezunu olan Aziz Akgül, Orta Doğu Teknik Üniversitesi işletme bölümünde MBA ve ABD Naval Postgraduate School\'da MS yaptı. Ankara Üniversitesi işletme bölümünde doktorasını yaptı.
+
+			KOSGEB Başkanlığı, Başbakan Başmüşavirliği ve milletvekilliği yapmıştır. Hâlen kurucusu olduğu Türkiye İsrafı Önleme Vakfı\'nda, mütevelli heyeti başkanı olarak yoksullara yönelik mikro kredi veren Türkiye Grameen Mikrofinans Programı\'na öncülük etmektedir. İşletme, Yöneylem Araştırması, İstatistik, Savunma Sanayi ve Hepitalizm üzerine 17 kitap ve çok sayıda makalesi bulunmaktadır.',
+			'en' => 'He graduated from the Department of Business Administration of the Turkish Military Academy. He earned an MBA from the Middle East Technical University, Department of Business Administration and an MS from the Naval Postgraduate School of the USA. He completed his doctorate at Ankara University, Faculty of Political Sciences, Department of Business Administration.
+
+			He served as KOSGEB President, Prime Minister\'s Chief Advisor and Member of Parliament. He still leads the Turkish Grameen Microfinance Program, which provides microcredit for the poor, as the Chairman of the Board of Trustees of the Turkish Waste Prevention Foundation, of which he is the founder. He has published 17 books and numerous articles on Business, Operations Research, Statistics, Defense Industry and Happytalism.'
+		),
+		'type' => array(
+			array(
+				'type' => 'founder',
+				'order' => 1
+			),
+			array(
+				'type' => 'board-of-directors',
+				'order' => 9
+			)
+		)
+	)
+);
